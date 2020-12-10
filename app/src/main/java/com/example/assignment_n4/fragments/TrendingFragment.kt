c54fe412f59eb506c6fac59afd5184b4e11d3b04
Luka Kldiashvili <42884387+lukakldiashvili@users.noreply.github.com>
2020-12-10 16:45:43 +0400

    Added trending fragment

---
+++ app/src/main/java/com/example/assignment_n4/fragments/TrendingFragment.kt
@@ -0,0 +1,19 @@
+package com.example.assignment_n4.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.assignment_n4.R
+
+class TrendingFragment: Fragment(R.layout.fragment_trending)  {
+
+    private lateinit var textView: TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
